cfc42b63e99a6b1b1c2c02625d504a9fa8f7414a
Anton Tantsev <tochta4@gmail.com>
2019-03-12 00:44:29 +0300

    Sixth сommit

---
+++ task2/ex1.js
@@ -0,0 +1,9 @@
+function determineType(arg) {
+    if ((typeof arg === "number") || (typeof arg === "string")) {
+        return typeof arg;
+    } else {
+        return "undefined";
+    }
+    
+}
+console.log(determineType(""));
