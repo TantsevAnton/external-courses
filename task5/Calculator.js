667d5ecff5dfce6963536db0cec644b6f5c0d694
Anton Tantsev <tochta4@gmail.com>
2019-03-21 22:00:13 +0300

    Twenty ninth commit

---
+++ task5/Calculator.js
@@ -0,0 +1,97 @@
+var Calculator = {
+
+    result: 0,
+
+    getResult: function() {
+        return this.result;
+    },
+
+    reset: function() {
+        this.result = 0;
+        return this.result;
+    },
+
+    add: function(arg) {
+        if (typeof arg === "undefined"){
+            arg = 0;           
+        }
+
+        this.result = this.result + arg;
+
+        return function f(arg) {
+            if (typeof arg === "undefined"){
+                arg = 0;           
+            }
+
+            Calculator.result = Calculator.result + arg;
+
+            return f;
+        }
+    },
+
+    subtract: function(arg) {
+        if (typeof arg === "undefined"){
+            arg = 0;           
+        }
+
+        this.result = this.result - arg;
+
+        return function f(arg) {
+            if (typeof arg === "undefined"){
+                arg = 0;           
+            }
+
+            Calculator.result = Calculator.result - arg;
+
+            return f;
+        }
+    },
+
+    divide: function(arg) {
+        if (typeof arg === "undefined"){
+            arg = 1;           
+        }
+
+        this.result = this.result / arg;
+
+        return function f(arg) {
+            if (typeof arg === "undefined"){
+                arg = 1;           
+            }
+
+            Calculator.result = Calculator.result / arg;
+
+            return f;
+        }
+    },
+
+    multiply: function(arg){
+        if (typeof arg === "undefined"){
+            arg = 1;           
+        }
+
+        this.result = this.result * arg;
+
+        return function f(arg) {
+            if (typeof arg === "undefined"){
+                arg = 1;           
+            }
+
+            Calculator.result = Calculator.result * arg;
+
+            return f;
+        }
+    }
+}
+
+console.log(Calculator.getResult()); // 0
+
+Calculator.add(false)();
+
+console.log(Calculator.getResult()); // 0
+
+Calculator.subtract()(2)()();
+Calculator.multiply()(2)(3)();
+Calculator.divide()(2);
+
+console.log(Calculator.getResult()); // 0
